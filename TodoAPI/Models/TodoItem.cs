7c052218cdb956b00adc72b72b302b6e39168c28
HUANG,XUANHUI <huangxh8531@gmail.com>
2024-09-04 15:39:02 +0800

    增加註解

--- TodoAPI/Models/TodoItem.cs
+++ TodoAPI/Models/TodoItem.cs
@@ -2,6 +2,10 @@
 {
     public class TodoItem
     {
+        /*
+         * 對應資料庫表格
+         */
+
         public long Id { get; set; }
         public string? Name { get; set; }
         public bool IsComplete { get; set; }
