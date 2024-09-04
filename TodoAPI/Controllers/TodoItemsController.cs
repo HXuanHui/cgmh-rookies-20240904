2cceaa44566613d4cc077213411730d094461777
HUANG,XUANHUI <huangxh8531@gmail.com>
2024-09-04 16:41:42 +0800

    新增 PostTodoItem方法

--- TodoAPI/Controllers/TodoItemsController.cs
+++ TodoAPI/Controllers/TodoItemsController.cs
@@ -152,5 +152,15 @@ namespace TodoAPI.Controllers
         {
             return _context.TodoItems.Any(e => e.Id == id);
         }
+
+        [HttpPost]
+        public async Task<ActionResult<TodoItem>> PostTodoItem(TodoItem todoItem)
+        {
+            _context.TodoItems.Add(todoItem);
+            await _context.SaveChangesAsync();
+
+            //    return CreatedAtAction("GetTodoItem", new { id = todoItem.Id }, todoItem);
+            return CreatedAtAction(nameof(GetTodoItem), new { id = todoItem.Id }, todoItem);
+        }
     }
 }
