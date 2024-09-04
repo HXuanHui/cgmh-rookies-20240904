36c2df3dfa7b01f5c331a24676c0855232f1e2b2
HUANG,XUANHUI <huangxh8531@gmail.com>
2024-09-04 17:13:16 +0800

    新增資料庫內容

---
+++ TodoAPI/Models/TodoContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TodoAPI.Models;
+
+namespace TodoAPI.Models
+{
+    public class TodoContext : DbContext
+    {
+        /*
+        * 資料庫內容
+        */
+        public TodoContext(DbContextOptions<TodoContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<TodoItem> TodoItems { get; set; } = null!;
+    }
+}
